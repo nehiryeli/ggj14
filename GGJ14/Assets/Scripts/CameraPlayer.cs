125b3616ef67fd29d7b381c39ba2ceb64def825c
Stanislav Ursache <d34dkn16h7@gmail.com>
2014-01-24 23:21:48 +0200

    init2

---
+++ GGJ14/Assets/Scripts/CameraPlayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPlayer : MonoBehaviour
+{
+	void FixedUpdate ()
+    {
+        transform.Translate(Vector3.forward * .1f);
+	}
+}
---
+++ GGJ14/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+    public string playerName;
+
+    public KeyCode left, right;
+
+    private int moveSpaceMult = 2;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(left))
+            if (transform.localPosition.x > -4)
+                transform.Translate(Vector3.left * moveSpaceMult);
+
+        if (Input.GetKeyDown(right))
+            if (transform.localPosition.x < 4)
+                transform.Translate(Vector3.right * moveSpaceMult);
+    }
+}
